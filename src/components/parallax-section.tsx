6c614dc4566cf28fe6180d9fac618fe55ce1bbd0
Eugene <dev@hyperion.cy>
2024-10-16 23:23:02 +0200

    fix parallax section

--- src/components/parallax-section.tsx
+++ src/components/parallax-section.tsx
@@ -6,7 +6,7 @@ import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax'
 
 export const ParallaxSection = ({ imageSrc }: { imageSrc: string }) => {
   return (
-    <section className="mx-auto">
+    <section className="w-full">
       <ParallaxBanner className="h-[480px] md:h-[600px] relative">
         <ParallaxBannerLayer expanded={false} speed={-10}>
           <Image fill src={getUploadCareUrl({ src: imageSrc })} alt="Image" style={{ objectFit: 'cover' }} />
