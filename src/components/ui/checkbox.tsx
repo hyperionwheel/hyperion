0d2553e85739805cbe0e4bb90118573a79eb67f4
Eugene <dev@hyperion.cy>
2024-10-29 17:12:58 +0100

    adjust checkbox

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import * as React from 'react'
+import React, { useId } from 'react'
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
 
@@ -8,20 +8,39 @@ import { cn } from '@/lib/utils'
 
 export const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    className={cn(
-      'peer h-2.5 w-2.5 shrink-0 rounded-[4px] transition-colors border border-transparent bg-white shadow focus-visible:border-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:text-disabled disabled:bg-[#F5F5F5] data-[state=checked]:bg--main data-[state=checked]:text-primary-main',
-      className
-    )}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
-      <CheckIcon className="h-2.5 w-2.5" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-))
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & {
+    label: React.ReactNode | string
+    error?: boolean
+  }
+>(({ className, label, error, ...props }, ref) => {
+  const id = useId()
+
+  return (
+    <div className="flex space-x-2">
+      <CheckboxPrimitive.Root
+        id={id}
+        ref={ref}
+        className={cn(
+          'peer h-2.5 w-2.5 shrink-0 rounded-[4px] transition-colors border border-transparent bg-white shadow focus-visible:border-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:text-disabled disabled:bg-[#F5F5F5] data-[state=checked]:bg--main data-[state=checked]:text-primary-main',
+          {
+            'border-error': error,
+          },
+          className
+        )}
+        {...props}
+      >
+        <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
+          <CheckIcon className="h-2.5 w-2.5" />
+        </CheckboxPrimitive.Indicator>
+      </CheckboxPrimitive.Root>
+      <label
+        htmlFor={id}
+        className="text-[8px] leading-[10px] cursor-pointer text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70 sm:text-[12px] sm:leading-[16px] md:leading-[20px]"
+      >
+        {label}
+      </label>
+    </div>
+  )
+})
 
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
