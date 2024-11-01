48f7eab70684879f53287fcfc54dc07b61b16130
Eugene <dev@hyperion.cy>
2024-11-01 13:03:34 +0100

    add link to cookie

--- src/components/cookie-consent.tsx
+++ src/components/cookie-consent.tsx
@@ -6,6 +6,7 @@ import { Button } from './ui/button'
 import { useEffect, useState } from 'react'
 import { cn, getLocalStorage, setLocalStorage } from '@/lib/utils'
 import { COOKIE_CONSENT_KEY } from '@/lib/constants'
+import { Link } from '@/i18n/routing'
 
 export const CookieConsent = () => {
   const t = useTranslations('cookie_consent')
@@ -40,7 +41,15 @@ export const CookieConsent = () => {
       <div className="flex flex-col gap-2 mx-auto py-2 px-1.25 items-center max-w-[calc(100%_-_20px)] sm:max-w-[480px] my-3 shadow-[0px_2px_8px_0px_#00000033] bg-white rounded-md sm:my-3.75 sm:gap-0 sm:flex-row sm:p-1.25">
         <div className="flex flex-1 flex-col gap-1 items-center sm:flex-row sm:gap-2">
           <CookieIcon />
-          <span className="flex sm:text-[14px] sm:leading-[22px]">{t('label')}</span>
+          <span className="sm:text-[14px] sm:leading-[22px]">
+            {t.rich('label', {
+              privacy: (chunks) => (
+                <Link className="underline" href="/privacy" target="_blank">
+                  {chunks}
+                </Link>
+              ),
+            })}
+          </span>
         </div>
         <div className="flex flex-col w-full sm:mt-0 sm:w-auto sm:flex-row gap-0.5">
           <Button className="w-full sm:w-auto" size="sm" variant="outlined" onClick={() => handleConsent(false)}>
